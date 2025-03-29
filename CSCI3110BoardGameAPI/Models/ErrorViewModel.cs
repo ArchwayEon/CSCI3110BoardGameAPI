1a138065a80b1e210b17d41ec45fe8754be4815c
Jeff Roach <roachj@etsu.edu>
2025-03-29 05:36:35 -0400

    Add project files.

---
+++ CSCI3110BoardGameAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSCI3110BoardGameAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
